17b4c42984b422827050e87d2ca01012b7367acf
LMsqz <luisfmasaquiza@hotmail.com>
2023-04-29 21:13:44 +0200

    tersara commit

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
-import './style.css'
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {getDatabase, ref, push, onValue} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+const appSettings = {
+  databaseURL : "https://app-compras-4dc7b-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
+const shoppingListEl = document.getElementById('shopping-list');
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value;
+
+    push(shoppingListInDB, inputValue);
+
+    clearInputFieldEd()
+
+    // appendItemToShoopingListEL(inputValue)
+    
+   
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+  let itemsArray = Object.values(snapshot.val())
+
+  clearShoppingListEl()
+  
+  for (let i = 0; i < itemsArray.length; i++) {
+    appendItemToShoopingListEL(itemsArray[i])
+  }
+})
+
+function clearShoppingListEl(){
+  shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEd(){
+  inputFieldEl.value = ""
+}
+
+function appendItemToShoopingListEL(itemValue){
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
